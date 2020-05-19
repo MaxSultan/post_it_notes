11a5cb26218dbd211e9ba5e1dde602bc9530faa7
Max Sultan <maxsultakent@gmail.com>
2020-05-18 18:50:48 -0600

    centered pages

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,13 +1,2 @@
 class Note < ApplicationRecord
-    class CreateNotes < ActiveRecord::Migration[5.1]
-        def change
-          create_table :notes do |t|
-            t.string :title
-            t.string :author
-            t.text :body
-      
-            t.timestamps
-          end
-        end
-      end
 end
